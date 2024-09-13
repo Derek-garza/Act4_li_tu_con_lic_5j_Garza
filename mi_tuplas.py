dd74d57ac1c8a3d34ee17f2565d8da829038c2ec
Derek-garza <a.22308051281196@cbtis128.edu.mx>
2024-09-13 09:54:52 -0600

    Update mi_tuplas.py

--- mi_tuplas.py
+++ mi_tuplas.py
@@ -1,12 +1,12 @@
 print("Ejemplo de tuplas")
-canciones=("Te amo","El NoaNoa","Amor eterno")
+canciones=("el triste","el carro rojo","Amor eterno")
 print(canciones)
 x = ("apple", "banana", "cherry")
 y = list(canciones)
 print(y)
-y[1] = "La puerta negra"
+y[1] = "La loca de san blas"
 x = tuple(y)
 print(x)
 print("Listado de elementos de la tupla canciones con ciclo for")
-for Garcia in canciones:
-    print(Garcia)
+for Garza in canciones:
+    print(Garza)
